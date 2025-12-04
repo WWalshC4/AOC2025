a3acb16a9c4c93f65695fae41df2225b2549d894
William Walsh <wwalsh@control4.com>
2025-12-04 07:11:10 +0000

    day04 complete

---
+++ day04/day04b.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import os
+
+try:
+    inputFile = open("./input.txt", "r")
+    data = inputFile.read()
+    inputFile.close()
+except:
+    pass
+
+test = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+# removableRolls = 43
+
+# data = test
+
+grid = []
+
+lines = data.splitlines()
+
+for line in lines:
+    gridline = list(line)
+    grid.append(gridline)
+
+# for gridline in grid:
+#    print("".join(gridline))
+
+maxY = len(grid)
+maxX = len(grid[0])
+
+removableRolls = 0
+
+
+def removeAccessibleRolls():
+    removedRolls = 0
+    for yPos, row in enumerate(grid):
+        for xPos, position in enumerate(row):
+            if position == "@":
+                adjacentRolls = 0
+                for xDiff in range(-1, 2):
+                    for yDiff in range(-1, 2):
+                        if xDiff == 0 and yDiff == 0:
+                            continue
+                        newX = xPos + xDiff
+                        newY = yPos + yDiff
+                        if 0 <= newX < maxX and 0 <= newY < maxY:
+                            if grid[newY][newX] == "@":
+                                adjacentRolls += 1
+                if adjacentRolls < 4:
+                    grid[yPos][xPos] = "x"
+                    removedRolls += 1
+    return removedRolls
+
+
+removedARoll = True
+while removedARoll:
+    removedRolls = removeAccessibleRolls()
+    # for gridline in grid:
+    #   print("".join(gridline))
+    removableRolls += removedRolls
+    if removedRolls == 0:
+        removedARoll = False
+
+print(removableRolls)
